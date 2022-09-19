7952c80c7f424af3c67d181cac978d70e7a9604e
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-09-19 10:43:48 +0700

    fixed?

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,15 @@
     let tablinks = document.getElementsByClassName("tablinks");
     let tabcontent = document.getElementsByClassName("tabcontent");
 function openCity(evt, tabName) {
-    // Declare all variables
     var i;
-    //WORK ON THIS
-    // Get all elements with class="tabcontent" and hide them
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
   
-    // Get all elements with class="tablinks" and remove the class "active"
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
   
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
   }
@@ -33,6 +28,6 @@ function openCity(evt, tabName) {
 $('.flash-slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 4000
 })
