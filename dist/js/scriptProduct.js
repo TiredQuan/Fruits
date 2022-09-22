aa9e2917e088bebc85cf564a9f31d1e0c5bc79b5
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-09-22 17:29:09 +0700

    a

---
+++ dist/js/scriptProduct.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".product-gallery").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayHoverPause:true,
+        URLhashListener:true,
+    });
+  });
+
+
+
