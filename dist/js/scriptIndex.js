a6d4fcdb99a8ccc365471eb35c97c1e408010a43
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-09-26 17:25:57 +0700

    done

--- dist/js/scriptIndex.js
+++ dist/js/scriptIndex.js
@@ -1,22 +1,3 @@
-    let tablinks = document.getElementsByClassName("tablinks");
-    let tabcontent = document.getElementsByClassName("tabcontent");
-function openCity(evt, tabName) {
-    var i;
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-  
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-
-
-  tabcontent[0].style.display = "block"
-
 
   $('.slick-slider').slick({
     slidesToShow: 1,
